35cb8539e9b1fbccbe1c7aeebb5c5d3e5cef1dd7
P3p1d <46057981+P3p1d@users.noreply.github.com>
2018-12-23 14:06:51 +0100

    opraveny predpony

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 
 def get_prefix(bot, message):
     """A callable Prefix for our bot. This could be edited to allow per server prefixes."""
-    prefixes = ['test§']
+    prefixes = ['§','debile ']
     #if not message.server:
     #    # pouze vykricnik mimo server
     #    return '!'

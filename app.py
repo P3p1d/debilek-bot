1bf9b40df443061f2d0177131e06ada3b3c5d35c
P3p1d <46057981+P3p1d@users.noreply.github.com>
2018-12-27 20:09:31 +0100

    pridano updatedbl do cogs seznamu

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ bot = discord.Client()
 bot = commands.Bot(command_prefix=get_prefix)
 bot.remove_command('help')
 #()  []  {} `
-init_extensions = ['cogs.fun','cogs.wiki','cogs.help','cogs.money','cogs.error_handler','cogs.images','cogs.Music','cogs.ascii_art','cogs.nasa']
+init_extensions = ['cogs.fun','cogs.wiki','cogs.help','cogs.money','cogs.error_handler','cogs.images','cogs.Music','cogs.ascii_art','cogs.nasa','cogs.updatedbl']
 
 if __name__ == '__main__':
     for extension in init_extensions:

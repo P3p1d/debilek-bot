88974b552fafa6421907f35bee382ed1497aaae7
P3p1d <46057981+P3p1d@users.noreply.github.com>
2018-12-29 19:30:13 +0100

    odstranen nefunkční dbl update
    jednou to opravím přísahám

--- app.py
+++ app.py
@@ -72,13 +72,7 @@ async def quit(ctx):
     else:
         await bot.say("Tak čau!")
         await bot.logout()
-#------------------------------------------------------------------
-url = f"https://discordbots.org/api/bots/485115987000295435/stats"
-payload = {"server_count": str(len(bot.servers))}
-headers = {"Authorization": os.environ["dblTOKEN"]}
-r = requests.post(url, data=payload, headers=headers)
-print("[+] Guild change detected, posting guild count to DBL")
-#-------------------------------------------------------------------
+
 @bot.event
 async def on_ready():
     print('Online jako:')

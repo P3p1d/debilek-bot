3c0245e127e8aa4e0e0df359ebb2cc2b912aa950
P3p1d <46057981+P3p1d@users.noreply.github.com>
2019-01-03 13:51:39 +0100

    import math.py

--- app.py
+++ app.py
@@ -29,7 +29,7 @@ bot = discord.Client()
 bot = commands.Bot(command_prefix=get_prefix)
 bot.remove_command('help')
 #()  []  {} `
-init_extensions = ['cogs.fun','cogs.wiki','cogs.help','cogs.money','cogs.error_handler','cogs.images','cogs.Music','cogs.ascii_art','cogs.nasa','cogs.Reddit']
+init_extensions = ['cogs.fun','cogs.wiki','cogs.help','cogs.money','cogs.error_handler','cogs.images','cogs.Music','cogs.ascii_art','cogs.nasa','cogs.Reddit','cogs.math']
 
 if __name__ == '__main__':
     for extension in init_extensions:

e1d855b87203aad682e5a68c9b28c0076d166482
P3p1d <46057981+P3p1d@users.noreply.github.com>
2019-01-03 13:54:44 +0100

    Update math.py

--- cogs/math.py
+++ cogs/math.py
@@ -1,18 +1,52 @@
+import os
+import re
+import math
+import asyncio
+import discord
 import numpy as np
 import matplotlib.pyplot as plt
-x=np.array(np.linspace(-100,100,1000))
-var = {'x':x,'sin':np.sin,'cos':np.cos,'sqrt':np.sqrt,'log':np.log,'abs':np.abs}
-a="x**b"
-if "x" not in a:
-	a = "x-x+"+a
-try:
-	plt.plot(x, eval(a,var),"r-")
-except OverflowError:
-	print("Čísla jsou moc velká")
-except NameError as e:
-	print("Zadal jsi neznámou, kterou opravdu neznám")
-	print(e)
-plt.grid(True, which='both')
-plt.axhline(y=0, color='k')
-plt.axvline(x=0, color='k')
-plt.show()
+from discord.ext import commands
+
+class TimedOutExc(Exception):
+	pass
+class Math:
+	def __init__(self,bot):
+
+		self.bot = bot
+	async def getgraph(self,ctx,args):
+		x=np.array(np.linspace(-100,100,1000))
+		var = {'x':x,'sin':np.sin,'cos':np.cos,'sqrt':np.sqrt,'log':np.log,'abs':np.abs,'pow':np.power}
+		if "**" in args:
+			return await self.bot.send_message(ctx.message.channel,"Neplatný ** operátor, použij pow()")
+		if "x" not in args:
+			args= "x-x+"+args
+		try:
+			plt.plot(x, eval(args,var),"r-")
+		except FloatingPointError:
+			return await self.bot.send_message(ctx.message.channel,"Výpočet trval příliš dlouho")
+		except OverflowError:
+			return await self.bot.send_message(ctx.message.channel,"Čísla jsou moc velká")
+		except NameError as e:
+			print(e)
+			return await self.bot.send_message(ctx.message.channel,"Zadal jsi neznámou, kterou opravdu neznám")
+		except SyntaxError:
+			return await self.bot.send_message(ctx.message.channel,"Někde máš překlep (možná chybějící závorka?)")
+		except ZeroDivisionError:
+			return await self.bot.send_message(ctx.message.channel,"`∞`")
+		plt.grid(True, which='both')
+		plt.axhline(y=0, color='k')
+		plt.axvline(x=0, color='k')
+		filename = str(ctx.message.server.id)+".jpg"
+		plt.savefig(filename,bbox_inches='tight')
+		plt.gcf().clear()
+		await self.bot.send_file(ctx.message.channel,filename)
+		os.remove(filename)
+
+	@commands.command(pass_context=True,aliases=["y=","graf"])
+	async def graph(self,ctx,*args):		
+		await self.bot.send_typing(ctx.message.channel)	
+		np.seterr(over="raise",divide="raise")
+		args = ' '.join(args)
+		await self.getgraph(ctx,args)
+def setup(bot):
+	bot.add_cog(Math(bot))

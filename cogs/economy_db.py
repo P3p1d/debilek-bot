37d8a1a12f363ce41101931ed401242570eeeec2
P3p1d <46057981+P3p1d@users.noreply.github.com>
2019-01-12 14:31:18 +0100

    biz update1

--- cogs/economy_db.py
+++ cogs/economy_db.py
@@ -3,12 +3,14 @@ import random
 import discord
 import datetime
 from pymongo import *
+from collections import Counter
 from discord.ext import commands
 
 class Economy:
 	def __init__(self,bot):
 		self.bot = bot
-		self.d=MongoClient(os.environ["MONGOURI"]).debilek
+		self.col=MongoClient(os.environ["MONGOURI"])
+		self.d=self.col.debilek
 	@commands.command(pass_context = True,no_pm=True,aliases=["ekonomy","ekonomika","€","balance","bilance"])
 	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
 	async def economy(self,ctx,user:discord.Member = None):
@@ -19,9 +21,14 @@ class Economy:
 		acc = self.d[server].find_one({"name":str(user)})
 		if acc is None:
 			self.d[server].insert_one({"name":str(user),"amount":500})
-			await self.bot.say(f"`Účet pro {user.display_name} byl založen!`")
-		else:
-			await self.bot.say(f"`{user.display_name} má na účtě {acc['amount']} lováčů`")
+			return await self.bot.say(f"`Účet pro {user.display_name} byl založen!`")
+		
+		if "pers" in acc:
+			t = (datetime.datetime.utcnow()-acc["last_check"]).total_seconds()
+			acc["amount"] += t*acc["pers"]
+			self.d[server].update_one({"name":str(user)},{"$set":{"last_check":datetime.datetime.utcnow(),"amount":acc["amount"]}})
+			return await self.bot.say(f"`{user.display_name} má na účtě {round(acc['amount'],2)} penízků a vydělává {acc['pers']} za vteřinu`")
+		await self.bot.say(f"`{user.display_name} má na účtě {acc['amount']} penízků`")
 	
 	@commands.command(pass_context = True,no_pm=True,aliases=["denne","deni","dailyscheckel","neetbux","šekel"])
 	@commands.cooldown(rate=1, per=10, type=commands.BucketType.user)
@@ -114,6 +121,72 @@ class Economy:
 		won = mlt*amount
 		self.d[server].update_one({"name":str(ctx.message.author)},{"$set":{"amount":a["amount"]+won}})
 		await self.bot.say(f"`{ctx.message.author.display_name} vyhrál v automatu {won} šekelů!`")
+	
+	@commands.command(pass_context = True,no_pm=True,aliases=["biz"])
+	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
+	async def business(self,ctx):
+		await self.bot.send_typing(ctx.message.channel)
+		point = self.col.biz.bizdb.find({})
+		e=discord.Embed(colour=discord.Colour.green())
+		for doc in point:
+			e.add_field(name=doc["name"],value=f'id: {doc["id"]}\ncena: {doc["price"]}:dollar:\n{doc["des"]}',inline=False)
+		e.set_author(name="Byznys")
+		e.set_footer(text="Byznys si koupíš pomocí §buy <id>")
+		await self.bot.say(embed=e)
+	
+	async def getpers(self,a):
+		val = 0
+		counted=Counter(a["bizs"])
+		for biz,num in counted.items():
+			biz=self.col.biz.bizdb.find_one({"id":biz})
+			val+=biz["pers"]*num
+		return val
+	
+	@commands.command(pass_context = True,no_pm=True,aliases=["kup"])
+	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
+	async def buy(self,ctx,bizid:int=0):
+		await self.bot.send_typing(ctx.message.channel)
+		server = str(ctx.message.server.id)
+		if bizid==0:
+			return await self.bot.say("Neřekl jsi, co si chceš koupit!")
+		elif bizid>9:
+			return await self.bot.say("Zdal jsi moc vysoké id!")
+		biz = self.col.biz.bizdb.find_one({"id":bizid})
+		if biz is None:
+			return await self.bot.say("Tento byznys neexistuje!")
+		a = self.d[server].find_one({"name":str(ctx.message.author)})
+		if a is None:
+			return await self.bot.say("Ještě sis nezaložil účet!")
+		elif biz["price"]>a["amount"]:
+			return await self.bot.say("Na tento byznys nemáš peníze!")
+		self.d[server].update_one({"name":str(ctx.message.author)},{"$push":{"bizs":bizid}})
+		a = self.d[server].find_one({"name":str(ctx.message.author)})
+		upers = await self.getpers(a)																							
+		if "last_check" not in a:
+			self.d[server].update_one({"name":str(ctx.message.author)},{"$set":{"amount":a["amount"]-float(biz["price"]),"pers":upers,"last_check":datetime.datetime.utcnow()}})
+		else:
+			self.d[server].update_one({"name":str(ctx.message.author)},{"$set":{"amount":a["amount"]-float(biz["price"]),"pers":upers}})
+		await self.bot.say(f"Úspěšně sis koupil předmět {biz['name']}!")
+
+	@commands.command(pass_context = True,no_pm=True,aliases=["inventář"])
+	@commands.cooldown(rate=1, per=5, type=commands.BucketType.user)
+	async def inventory(self,ctx,user:discord.Member=None):
+		await self.bot.send_typing(ctx.message.channel)
+		server = str(ctx.message.server.id)
+		if user is None:
+			user=ctx.message.author
+		a = self.d[server].find_one({"name":str(user)})
+		if a is None:
+			return await self.bot.say(f"{user.display_name} si ještě nezaložil účet")
+		if "bizs" not in a:
+			return await self.bot.say(f"{user.display_name} si ještě nic nekoupil!")
+		counted=Counter(a["bizs"])
+		e=discord.Embed(colour=discord.Colour.green())
+		for biz,num in counted.items():
+			biz=self.col.biz.bizdb.find_one({"id":biz})
+			e.add_field(name=biz["name"],value=f"{num} krát\nVydělává {num*biz['pers']} za vteřinu",inline = False)
+		e.set_author(name=user.display_name,icon_url=user.avatar_url)
+		await self.bot.say(embed=e)
 
 def setup(bot):
 	bot.add_cog(Economy(bot))

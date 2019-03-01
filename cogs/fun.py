ee246548b18b817aee7cdeb7817d122aad0c417b
Pepid <46057981+P3p1d@users.noreply.github.com>
2019-03-01 17:08:15 +0100

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -11,7 +11,7 @@ from unidecode import unidecode
 from discord.ext import commands
 from discord.ext.commands import has_permissions
 
-class Fun:
+class Fun(commands.Cog):
 	def __init__(self,bot):
 		self.bot = bot
 

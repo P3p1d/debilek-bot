667e9bc478ef0a8b1df58d08299d4ddcc3697049
Pepid <46057981+P3p1d@users.noreply.github.com>
2020-03-23 02:10:15 +0100

    Update Music.py

--- cogs/Music.py
+++ cogs/Music.py
@@ -39,7 +39,7 @@ ytdlopts = {
 }
 
 ffmpegopts = {
-    'before_options': '-nostdin',
+    'before_options': '-nostdin -reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5"',
     'options': '-vn'
 }
 
